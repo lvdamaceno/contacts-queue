dcd609251a3893fb5e51828070f70c03ccd4af41
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2023-09-26 07:23:51 -0300

    init front

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,22 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 const app = fastify()
+const prisma = new PrismaClient()
 
-app.get('/', () => {
-  return 'Hello'
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+  return users
 })
 
 app.get('/admin', () => {
   return 'Admin'
 })
 
-app.listen({
-  port: 3333
-}).then(() => {
-  console.log('HTTP server running on http://localhost:3333')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running on http://localhost:3333')
+  })
