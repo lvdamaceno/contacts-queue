12c456a1ca7f1bb21de3c4150422393d7164aee4
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2023-09-29 17:47:32 -0300

    update cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,13 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import { contactsRoute } from './routes/contacts'
 
 const app = fastify()
 
+app.register(cors, {
+  // when in production, list the real urls how can connect to the data base
+  origin: true,
+})
 app.register(contactsRoute)
 
 app
