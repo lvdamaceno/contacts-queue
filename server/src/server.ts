801b51f9d784ab1f56c774874cce37db35199ed2
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2023-09-29 17:39:44 -0300

    update CRUD

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,9 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { contactsRoute } from './routes/contacts'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-  return users
-})
-
-app.get('/admin', () => {
-  return 'Admin'
-})
+app.register(contactsRoute)
 
 app
   .listen({
