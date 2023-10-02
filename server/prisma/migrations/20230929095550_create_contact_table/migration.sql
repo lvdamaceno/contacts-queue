9d00a1eed01b3bee4a68bb26bbe898d1dd7b6253
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2023-10-02 11:28:42 -0300

    update remove server

--- server/prisma/migrations/20230929095550_create_contact_table/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Contact" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "userId" TEXT NOT NULL,
-    "contactNumber" TEXT NOT NULL,
-    "isUsed" BOOLEAN NOT NULL DEFAULT false,
-    CONSTRAINT "Contact_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
