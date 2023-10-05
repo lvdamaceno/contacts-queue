35b085cbaf3985ae00abf0c7c9a44dc99aa46c6a
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2023-10-04 22:04:18 -0300

    Update undo <a> modification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,16 @@ export default async function Home() {
   const contactLink: string = await Message()
   return (
     <div className="h-screen bg-zinc-900 text-zinc-50 flex items-center justify-center">
-      {/* <a 
+      <a 
       href={contactLink}
       // rel="noopener noreferrer"
       className=" text-xl font-mono rounded-xl p-5 bg-red-600 hover:bg-red-500">
         Inicie uma conversa
-      </a> */}
+      </a>
       
-      <Link href={contactLink} replace>
+      {/* <Link href={contactLink} replace>
         Inicie uma conversa
-      </Link>
+      </Link> */}
 
     </div>
   )
