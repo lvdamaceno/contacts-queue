7b24b186ac9f473a5714611852b7697c53c99ed3
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2023-10-30 00:07:04 -0300

    update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import { Roboto_Flex as Roboto } from 'next/font/google'
 const roboto = Roboto({ subsets: ['latin'] , variable: '--font-roboto'})
 
 export const metadata: Metadata = {
-  title: 'Contacts Queue',
-  description: 'Get on the queue',
+  title: 'Fila de Atendimento',
+  description: 'Clique no botão e comece um novo atendimento',
 }
 
 export default function RootLayout({
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={`${roboto.variable}font-sans`}>{children}</body>
     </html>
   )
