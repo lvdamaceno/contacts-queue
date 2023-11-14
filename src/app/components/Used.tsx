66edc8e36e39996d2080f98e6473b13c0660e2a1
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2023-11-13 22:39:26 -0300

    updade Used.tsx revert

--- src/app/components/Used.tsx
+++ src/app/components/Used.tsx
@@ -1,8 +1,4 @@
-'use client'
-
-
 import { api } from '@/lib/api'
-import { useEffect, useState } from 'react'
 
 export async function countUsed() {
     const response = await api.get('/countused')
@@ -12,15 +8,10 @@ export async function countUsed() {
 
 export function Used() {
     const number = countUsed()
-
-    const [isClient, setIsClient] = useState(false)
-    useEffect(() => {
-        setIsClient(true)
-    }, [])
     // console.log('Used: ' + number)
     // return number
     return (
-    <p className="text-lg"> Contatos efetuados: {isClient ? number: number}
+    <p className="text-lg"> Contatos efetuados: {number}
     </p>
     )
 }
