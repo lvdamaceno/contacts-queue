2d172ea9049a9e0c5e4701430a989e452e0aee1a
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2023-11-13 23:55:13 -0300

    update Used.tsx

--- src/app/components/Used.tsx
+++ src/app/components/Used.tsx
@@ -1,4 +1,5 @@
 import { api } from '@/lib/api'
+import ReactDOM from 'react-dom'
 
 export async function countUsed() {
     const response = await api.get('/countused')
@@ -8,10 +9,6 @@ export async function countUsed() {
 
 export function Used() {
     const number = countUsed()
-    // console.log('Used: ' + number)
-    // return number
-    return (
-    <p className="text-lg"> Contatos efetuados: {number}
-    </p>
-    )
+    const usedContacts = number.then(value => value)
+    return <p className="text-lg">Contatos efetuados: {usedContacts}</p>
 }
--- src/app/counters/page.tsx
+++ src/app/counters/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="h-screen bg-zinc-900 text-zinc-50 flex flex-col gap-3 items-center justify-center font-mono">
     <Remaining/>
-    <Used/>
+    <Used />
     </div>
   )
 }
