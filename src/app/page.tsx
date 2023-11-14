642dc7a0722310dd64776a1d396063d588f33e0e
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2023-11-13 22:41:45 -0300

    update page.tsx removing counters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ import { Button } from './components/Button'
 export default function Home() {
   return (
     <div className="h-screen bg-zinc-900 text-zinc-50 flex flex-col gap-3 items-center justify-center font-mono">
-      <Used />
+      {/* <Used /> */}
       <Button />
-      <Remaining />
+      {/* <Remaining /> */}
     </div>
   )
 }
