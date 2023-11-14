99ec7ff436c87409604e2ae0fb36962f27dccdee
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2023-11-13 22:59:16 -0300

    create counters page

--- src/app/counters/page.tsx
+++ src/app/counters/page.tsx
@@ -1,9 +1,11 @@
-import { Button } from './components/Button'
+import { Remaining } from '../components/Remaining'
+import { Used } from '../components/Used'
 
 export default function Home() {
   return (
     <div className="h-screen bg-zinc-900 text-zinc-50 flex flex-col gap-3 items-center justify-center font-mono">
-      <Button />
+    <Remaining/>
+    <Used/>
     </div>
   )
 }
