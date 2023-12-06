9ee80757e1f587c0fbc61fe3d0ca53b02c30d4cb
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2023-12-06 10:11:44 -0300

    Update Redirect.tsx Message

--- src/app/components/Redirect.tsx
+++ src/app/components/Redirect.tsx
@@ -2,7 +2,7 @@ import { api } from '@/lib/api'
 
 export async function Redirect() {
     const whatsapp_link = "https://api.whatsapp.com/send?phone=55"
-    const message = "&text=%F0%9F%8E%89%20Ol%C3%A1,%20cliente%20da%20Casa%20Contente!%20%F0%9F%8F%A0%F0%9F%92%96%0A%0AA%20%F0%9F%96%A4%20Black%20Friday%20%F0%9F%96%A4%20j%C3%A1%20chegou%20por%20aqui%20e%20n%C3%B3s%20estamos%20super%20empolgados%20para%20ver%20voc%C3%AA%20aproveitando%20as%20nossas%20ofertas%20imperd%C3%ADveis!%20%F0%9F%9B%8D%EF%B8%8F%E2%9C%A8%0A%0AE%20tem%20mais...%20Ao%20fazer%20suas%20compras,%20voc%C3%AA%20concorre%20automaticamente%20ao%20nosso%20%F0%9F%8E%81%20sorteio%20de%20100.000%20reais%20em%20pr%C3%AAmios!%20%F0%9F%A4%91%F0%9F%A5%B3%20que%20ocorrer%C3%A1%20no%20dia%20%F0%9F%97%93%EF%B8%8F23%20de%20dezembro.%0A%0AN%C3%A3o%20fique%20de%20fora%20dessa!%20Estamos%20te%20esperando!%20%F0%9F%8F%83%F0%9F%92%A8"
+    const message = "&text=%F0%9F%8E%89%F0%9F%8F%A0%20Transforme%20seu%20lar%20neste%20Natal%20com%20a%20Casa%20Contente!%20%F0%9F%8E%84%F0%9F%9B%8B%EF%B8%8F%20Aproveite%20m%C3%B3veis%20e%20eletrodom%C3%A9sticos%20incr%C3%ADveis%20e%20concorra%20a%20R$100.000%20em%20pr%C3%AAmios!%20%F0%9F%A4%91%F0%9F%8E%81%20Sorteio%20em%2023/12.%20N%C3%A3o%20perca!%20%F0%9F%8C%9F%F0%9F%8E%85%20%0AEntre%20em%20contato%20comigo%20para%20saber%20mais!%20"
     
     const response = await api.get('/contacts')
     const responseData = response.data
